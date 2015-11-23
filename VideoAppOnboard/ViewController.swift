5f5e4725421a7cde0e1a7226702d003d1c0cf5b7
Harry Ng <harryworld@gmail.com>
2015-11-23 23:30:48 +0800

    Initial Commit

---
+++ VideoAppOnboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoAppOnboard
+//
+//  Created by Harry Ng on 23/11/15.
+//  Copyright © 2015 STAY REAL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
