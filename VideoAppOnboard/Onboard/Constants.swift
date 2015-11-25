9e46d12bc26228423986b6c94f4cf830c4b05709
Harry Ng <harryworld@gmail.com>
2015-11-25 23:59:33 +0800

    Add Image Assets to View

---
+++ VideoAppOnboard/Onboard/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  VideoAppOnboard
+//
+//  Created by Harry Ng on 23/11/15.
+//  Copyright © 2015 STAY REAL. All rights reserved.
+//
+
+import Foundation
+
+let kPermissionGranted = "permissionGranted"
---
+++ VideoAppOnboard/Onboard/OnboardViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardViewController.swift
+//  VideoAppOnboard
+//
+//  Created by Harry Ng on 23/11/15.
+//  Copyright © 2015 STAY REAL. All rights reserved.
+//
+
+import UIKit
+
+class OnboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
