28bc78763142f94f5a655ff1d19ca3970cea36fb
Harry Ng <harryworld@gmail.com>
2015-11-26 10:30:32 +0800

    Embed each icon in a bigger UIView

--- VideoAppOnboard/Onboard/OnboardViewController.swift
+++ VideoAppOnboard/Onboard/OnboardViewController.swift
@@ -10,8 +10,35 @@ import UIKit
 
 class OnboardViewController: UIViewController {
 
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var audioView: UIView!
+    @IBOutlet weak var photosView: UIView!
+    
+    @IBOutlet weak var cameraImageView: UIImageView!
+    @IBOutlet weak var audioImageView: UIImageView!
+    @IBOutlet weak var photosImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapCamera = UITapGestureRecognizer(target: self, action: "cameraTapped:")
+        cameraView.addGestureRecognizer(tapCamera)
+        let tapAudio = UITapGestureRecognizer(target: self, action: "audioTapped:")
+        audioView.addGestureRecognizer(tapAudio)
+        let tapPhotos = UITapGestureRecognizer(target: self, action: "photosTapped:")
+        photosView.addGestureRecognizer(tapPhotos)
+    }
+    
+    func cameraTapped(gesture: UITapGestureRecognizer) {
+        print("camera")
     }
 
+    func audioTapped(gesture: UITapGestureRecognizer) {
+        print("audio")
+    }
+    
+    func photosTapped(gesture: UITapGestureRecognizer) {
+        print("photos")
+    }
+    
 }
