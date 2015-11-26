5d22a06413451d12f0ae10e4b746c1a94d5ec37d
Harry Ng <harryworld@gmail.com>
2015-11-26 11:45:19 +0800

    Add PermissionScope

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'VideoAppOnboard' do
+  # pod 'SnapKit', '~> 0.15.0'
+  # pod 'HanekeSwift', :git => 'https://github.com/Haneke/HanekeSwift.git'
+  pod 'PermissionScope'
+end
+
--- VideoAppOnboard/Onboard/OnboardViewController.swift
+++ VideoAppOnboard/Onboard/OnboardViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import PermissionScope
 
 class OnboardViewController: UIViewController {
 
+    let pscope = PermissionScope()
+    
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var audioView: UIView!
     @IBOutlet weak var photosView: UIView!
@@ -21,6 +24,35 @@ class OnboardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        pscope.viewControllerForAlerts = self
+        pscope.addPermission(CameraPermission(),
+            message: "We want to use Camera")
+        pscope.addPermission(MicrophonePermission(),
+            message: "We want to use Microphone")
+        pscope.addPermission(PhotosPermission(),
+            message: "We want to save your memories")
+        
+        pscope.onAuthChange = { (finished, results) in
+            print("Request was finished with results \(results)")
+            for result in results {
+                // Animate button
+                if result.status == .Authorized {
+                    if result.type == .Camera {
+                        
+                    }
+                    if result.type == .Microphone {
+                        
+                    }
+                    if result.type == .Photos {
+                        
+                    }
+                }
+            }
+        }
+        pscope.onDisabledOrDenied = { results in
+            print("Request was denied or disabled with results \(results)")
+        }
+        
         let tapCamera = UITapGestureRecognizer(target: self, action: "cameraTapped:")
         cameraView.addGestureRecognizer(tapCamera)
         let tapAudio = UITapGestureRecognizer(target: self, action: "audioTapped:")
@@ -31,13 +63,16 @@ class OnboardViewController: UIViewController {
     
     func cameraTapped(gesture: UITapGestureRecognizer) {
         print("camera")
+        pscope.requestCamera()
     }
 
     func audioTapped(gesture: UITapGestureRecognizer) {
         print("audio")
+        pscope.requestMicrophone()
     }
     
     func photosTapped(gesture: UITapGestureRecognizer) {
+        pscope.requestPhotos()
         print("photos")
     }
     
